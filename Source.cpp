bef7d273df181b14d90d3a2a42667606cd1843ac
Mapleint <albertarlington21@gmail.com>
2020-02-02 16:02:50 -0800

    Improved Versatility

--- Source.cpp
+++ Source.cpp
@@ -1,29 +1,42 @@
 #include <stdio.h>
 
-double G_TMP;
-
+/*will get rid of the include later*/
+double G_TMP = 8.5;
+/* temperature */
+bool binswitch = true;
+/*bin switch, when off shows the dec*/
 char G_TMP_STR[16];
-bool Idont[10];
+/* the temperature's string         */
+char G_TMP_STR_DEC[16];
+/* the temperature's string decimal */
 
-char *decToBinary(unsigned short n) 
-{ 
-    // Size of an integer is assumed to be 16 bits 
-    for (int i = 15; i >= 0; i--) { 
-        int k = n >> i; 
-        if (k & 1) 
-            G_TMP_STR[i] = 49;
-        else
-            G_TMP_STR[i] = 48;
-    } 
-    return G_TMP_STR;
+/*
+ *Char dec to binary changes the values within a char* AND returns it
+ *hence the *char type. DON'T USE ON LONGER THINGS LONGER THAN 10 BITS
+ */
+char* decToBinary(unsigned short n, char *str)
+{
+        for (int i = 10; i >= 0; i--) {
+                int k = n >> i;
+                if (k & 1)
+                        str[i] = 49;
+                else
+                        str[i] = 48;
+        }
+        return str;
 }
 
-int main()
+/*PrintsChartoPtr, by recursively going. Don't use this on non Chars*/
+void print(char *str)
 {
-    decToBinary(8);
-    for(int i = 15; i >= 0; i--)
-    {
-        printf("%c", G_TMP_STR[i]);
-    }
-    return 0; 
+        for (int i = 9; i >= 0; i--)
+                printf("%c", str[i]);
+}
+
+void main()
+{
+        decToBinary(binswitch ? G_TMP : (int)(G_TMP * 1000) % 1000,               
+        binswitch ? G_TMP_STR : G_TMP_STR_DEC);
+
+        print(binswitch ? G_TMP_STR : G_TMP_STR_DEC);
 }
