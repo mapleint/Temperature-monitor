f8f858b8c2537973f8b2ba0254521c5a11a8c23f
Mapleint <albertarlington21@gmail.com>
2020-02-04 15:31:58 -0800

    fixed the indents

--- TempuratureMonitor.cpp
+++ TempuratureMonitor.cpp
@@ -25,20 +25,21 @@ char* decToBinary(unsigned short n, char *str)
 /*PrintsChartoPtr, by recursively going. Don't use this on non Chars*/
 void print(char *str)
 {
-    for (int i = 9; i >= 0; i--)
-        digitalWrite(i, str[i] == '1' ? HIGH : LOW);
+        for (int i = 9; i >= 0; i--)
+                digitalWrite(i, str[i] == '1' ? HIGH : LOW);
 }
 
 
 void setup() {
-    for(int i = 10; i>0; --i)
-        pinMode(i,OUTPUT);
-    pinMode(11, INPUT);
+        for(int i = 10; i>0; --i)
+                pinMode(i,OUTPUT);
+        pinMode(11, INPUT);
 }
 
 void loop() {
-    G_TMP = analogRead(A0);
-    binswitch = digitalRead(11);
-    decToBinary(binswitch ? G_TMP : (int)(G_TMP * 1000) % 1000, binswitch ? G_TMP_STR : G_TMP_STR_DEC);
-    print(binswitch ? G_TMP_STR : G_TMP_STR_DEC);
+        G_TMP = analogRead(A0);
+        binswitch = digitalRead(11);
+        decToBinary(binswitch ? G_TMP : (int)(G_TMP * 1000) % 1000,
+                    binswitch ? G_TMP_STR : G_TMP_STR_DEC);
+        print(binswitch ? G_TMP_STR : G_TMP_STR_DEC);
 }
