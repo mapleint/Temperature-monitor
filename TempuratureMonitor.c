a0685441ce24c17c65267d026783d963a447b09e
Mapleint <albertarlington21@gmail.com>
2020-02-29 00:43:32 -0800

    Update TempuratureMonitor.c

--- TempuratureMonitor.c
+++ TempuratureMonitor.c
@@ -1,53 +1,39 @@
 double G_TMP;
-/* temperature, read from the anolog */
-double B_TMP;
-/* temperature, but the zero value. */
-bool binswitch;
-/*bin switch, when off shows the dec*/
-char G_TMP_STR[16];
-/* the temperature's string         */
-char G_TMP_STR_DEC[16];
-/* the temperature's string decimal */
 
-/*
- *Char dec to binary changes the values within a char* AND returns it
- *hence the *char type. DON'T USE ON LONGER THINGS LONGER THAN 10 BITS
- */
-void decToBinary(unsigned short n, char *str)
-{
-        for (int i = 10; i >= 0; i--) {
-                int k = n >> i;
-                if (k & 1)
-                        str[i] = 49;
-                else
-                        str[i] = 48;
-        }
-}
-/*PrintsChartoPtr, by recursively going. Don't use this on non Chars*/
-void print(char *str)
-{
-        for (int i = 9; i >= 0; i--)
-                digitalWrite(i, str[i] == '1' ? HIGH : LOW);
-}
 
 
 void setup() {
-        for(int i = 10; i>0; --i)
-                pinMode(i,OUTPUT);
-        pinMode(11, INPUT);
-        pinMode(12, INPUT);
+        Serial.begin(9600);
+        pinMode(11, OUTPUT);
+        pinMode(12, OUTPUT);
         pinMode(13, OUTPUT);
 }
 
+double VoltToCelcius(double val)
+{
+        return -1481.98+sqrt(2196200+(1.8639-x)/(0.00000388));
+}
+
 void loop() {   
-        if((bool)digitalRead(12)){
-                B_TMP = analogRead(A0);
-        }
-        if((bool)digitalRead)
-                digitalWrite(13, HIGH);   
-        else
+        G_TMP = VoltToCelcius((5.0*analogRead(A0))/1023.0);
+        if(G_TMP < 4.4){
+                digitalWrite(11, HIGH);
+                digitalWrite(12, LOW);   
                 digitalWrite(13, LOW);
-        G_TMP = (double)analogRead(A0) - (double)B_TMP;
-        decToBinary(binswitch ? G_TMP : (int)(G_TMP * 1000) % 1000, binswitch ? G_TMP_STR : G_TMP_STR_DEC);
-        print(binswitch ? G_TMP_STR : G_TMP_STR_DEC);
+        }else if(G_TMP > 4.4 && G_TMP < 21.11){
+                digitalWrite(11, LOW);  
+                digitalWrite(12, HIGH);
+                digitalWrite(13, LOW);  
+        } else {
+                digitalWrite(11, LOW);  
+                digitalWrite(12, LOW);  
+                digitalWrite(13, HIGH);
+        }
+        Serial.print((5.0*analogRead(A0))/1023.0);
+        Serial.print(", ");
+        Serial.print(G_TMP);
+        Serial.print(", ");
+        Serial.print(G_TMP*1.8 + 32);
+        Serial.print("\n");
+        delay(100);
 }
