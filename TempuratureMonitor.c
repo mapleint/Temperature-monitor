9d066ab46a57123663454fd0f58312c3d5609779
Mapleint <albertarlington21@gmail.com>
2020-03-07 12:34:17 -0800

    Bad kiddo
    and should feel bad

--- TempuratureMonitor.c
+++ TempuratureMonitor.c
@@ -1,35 +1,29 @@
 double G_TMP; //declare global temp
-void setup() {
+void setup(){e
         Serial.begin(9600);
-        pinMode(11, OUTPUT);
-        pinMode(12, OUTPUT);
-        pinMode(13, OUTPUT);
+        for(int a = 11; a <=13; a++)
+                pinMode(a, OUTPUT);
 }
-double VoltToCelcius(double val) // phat function telling vals
+double MVoltToCelcius(double val) // phat function telling vals
 {
         return -1481.98+sqrt(2196200+(1.8639-val)/(0.00000388));
 }
-
 void loop() {   
-        G_TMP = VoltToCelcius((5.0*analogRead(A0))/1023.0); // set G_TMP
-                if(G_TMP < 4.4){
+        G_TMP = VoltToCelcius((5.0*analogRead(A0))/1023.0); // set 
+        digitalWrite(11, LOW);  
+        digitalWrite(12, LOW);
+        digitalWrite(13, LOW);
+                if (G_TMP <= 4.4)
                         digitalWrite(11, HIGH); // leds being set
-                        digitalWrite(12, LOW);   
-                        digitalWrite(13, LOW);
-                }else if(G_TMP > 4.4 && G_TMP < 21.11){
-                        digitalWrite(11, LOW);  
-                        digitalWrite(12, HIGH);
-                        digitalWrite(13, LOW);  
-                } else {
-                        digitalWrite(11, LOW);  
-                        digitalWrite(12, LOW);  
+                 else if (G_TMP > 4.4 && G_TMP < 21.11)
+                        digitalWrite(12, HIGH);  
+                 else
                         digitalWrite(13, HIGH);
-                } // end of led block
         Serial.print((5.0*analogRead(A0))/1023.0); //print raw voltage
-        Serial.print(", ");
+        Serial.print(", C ");
         Serial.print(G_TMP); // print centigrade
-        Serial.print(", ");
-        Serial.print(G_TMP*1.8 + 32); //print freedom
+        Serial.print(", F ");
+        Serial.print(G_TMP*1.8 + 32); //print freedom units
         Serial.print("\n");
-        delay(100); // limit to not overload the mc
+        delay(350);
 }
